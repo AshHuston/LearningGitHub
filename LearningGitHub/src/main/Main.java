c169551b05b6a8cfde28a0b1b1ca3e466d47fe77
hustonas <hustonas@IL-PC-144.uccc.uc.edu>
2018-01-23 18:43:57 -0500

    Test project. First commit.

---
+++ LearningGitHub/src/main/Main.java
@@ -0,0 +1,16 @@
+/***************************************
+ * Name:Ash Huston
+ * Assignment #:
+ * Course #, Section: Course 2045 Section 01
+ * Due Date: 1/23/2018
+ * Description: GitHub testing.
+ */
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("We are testing.");
+	}
+
+}
